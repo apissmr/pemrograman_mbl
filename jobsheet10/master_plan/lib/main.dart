17395ca82997d658683f1c83095f18cce420ce20
apissmr <hafizhrabbanim@gmail.com>
2024-11-10 07:56:48 +0700

    praktikum_1

---
+++ jobsheet10/master_plan/lib/main.dart
@@ -0,0 +1,16 @@
+import './views/plan_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MasterPlanApp());
+
+class MasterPlanApp extends StatelessWidget {
+  const MasterPlanApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     theme: ThemeData(primarySwatch: Colors.purple),
+     home: PlanScreen(),
+    );
+  }
+}
